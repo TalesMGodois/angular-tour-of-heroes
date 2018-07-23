764fbd3801677924d0104219c507d4304a3de1b5
Tales Marinho Godois <tales.godois@gmail.com>
2018-07-23 16:01:53 -0300

    Why: Step 6
    *
    
    Services
    
    *

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
+import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 
 @Component({
     selector: 'app-heroes',
@@ -8,7 +8,7 @@ import { HEROES } from '../mock-heroes';
     styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    heroes  = HEROES;
+    heroes: Hero[];
 
     selectedHero:Hero;
 
@@ -17,11 +17,19 @@ export class HeroesComponent implements OnInit {
         name:'Windstorm'
     };
 
-    constructor() { }
+    constructor(private heroService:HeroService) { }
 
     public onSelect(hero:Hero):void{
         this.selectedHero = hero;
     }
 
-    ngOnInit() {}
+    public initHeroes():void {
+        this.heroService
+            .getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.initHeroes();
+    }
 }
